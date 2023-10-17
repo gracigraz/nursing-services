878a002140716eab8370a04e20ba35f67f41b669
Graciela Manzanares <38384752+gracigraz@users.noreply.github.com>
2023-10-17 18:36:47 -0400

    updated footer JSX code

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,22 @@
 export const Footer = () => {
   return (
-    <>
-      <footer className="footer"></footer>
-    </>
+    <footer className="footer">
+      <h4 className="footer__heading"></h4>
+      <h4 className="footer__heading">Contacto</h4>
+      <h4 className="footer__heading">Sigueme</h4>
+      <nav>
+        <img
+          className="footer__logo-bottom"
+          src="../assets/logos/Logo-bandsite.svg"
+          alt="logo-bandsite"
+        />
+        <img
+          className="footer__logo-bottom"
+          src="../assets/logos/Logo-bandsite.svg"
+          alt="logo-bandsite"
+        />
+      </nav>
+      <p className="footer__copyright">Copyright © 2023 All Rights Reserved</p>
+    </footer>
   );
 };
